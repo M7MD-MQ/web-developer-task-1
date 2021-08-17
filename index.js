2b64f6bae55c018226254bdefd9971f2e946903e
Mohammed AlMuqbil <mohammedalmuqbil.code@gmail.com>
2021-08-17 04:39:58 +0300

    Link the form you have created to Mock API - Done

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 // TODO: Create a function to add books
+function addBook(book) {
+    event.preventDefault()
 
-// TODO: Create a function to view all books 
+    const bookData = {
+        title: book.title.value,
+        subtitle: book.subtitle.value,
+        author: book.author.value,
+        image: book.image.value
+    };
+  
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://611ade7d22020a00175a42dd.mockapi.io/Books");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(bookData));
 
-// TODO: Create a function to delete a book 
+    
+}
+
+// TODO: Create a function to view all books
+
+
+// TODO: Create a function to delete a book
+
