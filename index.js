eaac63756415f1890415e5898073f0bd5d471938
Mohammed AlMuqbil <mohammedalmuqbil.code@gmail.com>
2021-08-18 09:07:58 +0300

    Complete View Book Function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => { getBooksJson(); });
+
 // TODO: Create a function to add books
 function addBook(book) {
-    event.preventDefault()
+    event.preventDefault();
 
     const bookData = {
         title: book.title.value,
@@ -8,17 +11,51 @@ function addBook(book) {
         author: book.author.value,
         image: book.image.value
     };
-  
+
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "https://611ade7d22020a00175a42dd.mockapi.io/Books");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(bookData));
 
-    
+
 }
 
 // TODO: Create a function to view all books
+function getBooksJson() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://611ade7d22020a00175a42dd.mockapi.io/Books');
+    xhr.onload = function () {
+        if (this.status === 200) {
+            try {
+                const jsonObject = JSON.parse(this.responseText);
+                viewBooks(jsonObject);
+            } catch (e) {
+                console.warn("Error: JSON File");
+            }
+        } else {
+            console.warn("Error: Wrong Call");
+        }
+    }
+    xhr.send();
+}
 
+function viewBooks(json) {
+    const tableBody = document.getElementById('tableBoody');
+    for (var i = 0; i < json.length; i++) {
+        var row = `<tr>     
+                            <td>${i}</td>
+							<td>${json[i].title}</td>
+							<td>${json[i].subtitle}</td>
+							<td>${json[i].author}</td>
+                            <td>${json[i].image}</td>
+                            <td><button onclick="deleteBook()" class="btn btn-danger">Delete</button></td>
+					  </tr>`
+        
+        tableBody.innerHTML += row;
+    }
+}
 
 // TODO: Create a function to delete a book
+function deleteBook() {
 
+}
